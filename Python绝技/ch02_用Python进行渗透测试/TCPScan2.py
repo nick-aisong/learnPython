bc902c1295da5dafd9f936d1fba8b574a6e63ca0
nick-aisong <37009423+nick-aisong@users.noreply.github.com>
2019-09-29 10:15:21 +0800

    Update TCPScan2.py

--- Python绝技/ch02_用Python进行渗透测试/TCPScan2.py
+++ Python绝技/ch02_用Python进行渗透测试/TCPScan2.py
@@ -45,7 +45,7 @@ def main():
     parser.add_option('-p', dest='tgtPort', type='string', help='specify target port[s] separated by comma')
     (options, args) = parser.parse_args()
     tgtHost = options.tgtHost
-    tgtPorts = str(options.tgtPort).split(', ')
+    tgtPorts = str(options.tgtPort).split(',')
     if (tgtHost is None) | (tgtPorts[0] is None):
         print parser.usage
         exit(0)
@@ -53,3 +53,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+#[root@NickCOS72V1 python]# python TCPScan2.py -H www.baidu.com -p 80,443,5432
+#
+#[+] Scan Restuls for 104.193.88.123
+#[+]443/tcp open
+#[+]
+#[+]80/tcp open
+#[+] HTTP/1.1 400 Bad Request
+#
+#
+#[-]5432/tcp closed
